1d3368607fbf71f05e4a364f235295e863ef19be
M1kkoT <mikko.tanhola@metropolia.fi>
2021-10-05 13:22:57 +0300

    title

--- app/src/main/java/com/example/munkkistracker/MainActivity.java
+++ app/src/main/java/com/example/munkkistracker/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
 
 
 
