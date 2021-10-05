8e0ab6e9276537602fbb2b6901660a795bdde22a
M1kkoT <mikko.tanhola@metropolia.fi>
2021-10-05 12:03:18 +0300

    Munkkiclasseja

--- app/src/main/java/com/example/munkkistracker/Berliininmunkki.java
+++ app/src/main/java/com/example/munkkistracker/Berliininmunkki.java
@@ -1,4 +1,23 @@
 package com.example.munkkistracker;
 
 public class Berliininmunkki implements Munkki{
+    @Override
+    public int getRasva() {
+        return 0;
+    }
+
+    @Override
+    public int getKcal() {
+        return 0;
+    }
+
+    @Override
+    public float getSokeri() {
+        return 0;
+    }
+
+    @Override
+    public float getHinta() {
+        return 0;
+    }
 }
--- app/src/main/java/com/example/munkkistracker/Munkki.java
+++ app/src/main/java/com/example/munkkistracker/Munkki.java
@@ -2,4 +2,12 @@ package com.example.munkkistracker;
 
 public interface Munkki {
 
+    public float getRasva();
+
+    public int getKcal();
+
+    public float getSokeri();
+
+    public float getHinta();
+
 }
