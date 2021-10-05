26389e136d6625a07aceb2c05ffc479b8f7100b1
M1kkoT <mikko.tanhola@metropolia.fi>
2021-10-05 14:39:37 +0300

    lista tehty pvm löytyy

--- app/src/main/java/com/example/munkkistracker/PaivakirjaActivity.java
+++ app/src/main/java/com/example/munkkistracker/PaivakirjaActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class PaivakirjaActivity extends AppCompatActivity {
 
@@ -16,7 +20,7 @@ public class PaivakirjaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paivakirja);
         ListView lv = findViewById(R.id.lista);
-        ArrayList<String> paivat = new ArrayList<>();
+        ArrayList<String> paivat = new ArrayList<>(); //testikoodia listwieviä varten varten
         paivat.add("Maanantai");
         paivat.add("Tiistai");
         paivat.add("Keskiviikko");
@@ -24,6 +28,10 @@ public class PaivakirjaActivity extends AppCompatActivity {
         paivat.add("Perjantai");
         paivat.add("Lauantai");
         paivat.add("Sunnuntai");
+
+        String date = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date()); //tällä saa päivämäärän
+        TextView teksti = findViewById(R.id.textView4); //testitextview päivämäärälle
+        teksti.setText(date);
 
 
         lv.setAdapter(new  ArrayAdapter<String>(
