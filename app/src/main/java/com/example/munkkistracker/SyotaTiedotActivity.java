16585fd1a05f85669468574adeee869174313de2
sarao <saraok@metropolia.fi>
2021-10-05 13:37:55 +0300

    Syötä tiedot activity muokkaus

--- app/src/main/java/com/example/munkkistracker/SyotaTiedotActivity.java
+++ app/src/main/java/com/example/munkkistracker/SyotaTiedotActivity.java
@@ -3,6 +3,7 @@ package com.example.munkkistracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SyotaTiedotActivity extends AppCompatActivity {
 
@@ -10,6 +11,18 @@ public class SyotaTiedotActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_syota_tiedot);
+
+        TextView maara = findViewById(R.id.tv_maara);
+        maara.setText("Määrä");
+
+        TextView kappale = findViewById(R.id.tv_hinta);
+        kappale.setText("Hinta");
+
+        TextView syote = findViewById(R.id.tv_syota);
+        syote.setText("Syötä tiedot");
+
+        TextView arvio = findViewById(R.id.tv_arvosana);
+        arvio.setText("Arvosana");
 
     }
 }
