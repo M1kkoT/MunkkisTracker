83702a4064b31a21f4def7711b68f0b1251a765f
M1kkoT <mikko.tanhola@metropolia.fi>
2021-10-05 13:39:23 +0300

    added singleton MunkkiList

--- app/src/main/java/com/example/munkkistracker/MunkkiList.java
+++ app/src/main/java/com/example/munkkistracker/MunkkiList.java
@@ -1,4 +1,38 @@
 package com.example.munkkistracker;
 
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+// singleton luokka munkkilistan tallennukseen
+// lisää munkki -> MunkkiList.getInstance().getMunkit.add(object);
+// koko lista -> MunkkiList.getInstance().getMunkit()
+
 public class MunkkiList {
+    private List<Munkki> munkit;
+    private static final MunkkiList ourInstance = new MunkkiList();
+
+    public static MunkkiList getInstance() {
+
+
+        return ourInstance;
+    }
+
+    private MunkkiList() {
+        munkit = new ArrayList<>();
+
+
+    }
+
+    public List<Munkki> getMunkit() { //tällä saa koko listan
+
+        return munkit;
+    }
+
+    public Munkki getMunkki(int luku){ //tällä saa yhden munkin listasta indeksillä i
+        return munkit.get(luku);
+
+    }
+
 }
