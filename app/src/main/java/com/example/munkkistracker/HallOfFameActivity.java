3651bea5d292d06d771f2ccc9a6eda5b997326f2
sarao <saraok@metropolia.fi>
2021-10-09 21:10:01 +0300

    Hall of fame, kuvan vaihdon koodaus

--- app/src/main/java/com/example/munkkistracker/HallOfFameActivity.java
+++ app/src/main/java/com/example/munkkistracker/HallOfFameActivity.java
@@ -3,6 +3,7 @@ package com.example.munkkistracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HallOfFameActivity extends AppCompatActivity {
@@ -20,15 +21,21 @@ public class HallOfFameActivity extends AppCompatActivity {
         win.setText("WINNER");
 
         //sara testaa, lel
-        int i = 0;
-        double testi = (MunkkiList.getInstance().getMunkit().get(i).getHillo())+(MunkkiList.getInstance().getMunkit().get(i).getBerlin());
+        //muuttujia muistiin
+        double berm = 0;
+        double hillom = 0;
+        double rinkelim = 0;
+        double kaikki = berm + hillom + rinkelim;
 
-        TextView test = findViewById(R.id.hall_testi);
-        test.setText(String.valueOf(testi));
-
-
-
-
+        //Vaihtaa kuvaa sen mukaan mitä munkkia on eniten
+        ImageView winner = findViewById(R.id.image_winner);
+        if (berm > hillom && berm > rinkelim){
+            winner.setImageResource(R.drawable.winner_ber);
+        }else if(hillom > berm && hillom > rinkelim){
+            winner.setImageResource(R.drawable.winner_hillo);
+        }else if (rinkelim > berm && rinkelim > hillom){
+            winner.setImageResource(R.drawable.winner_rinkeli);
+        }
 
     }
 }
