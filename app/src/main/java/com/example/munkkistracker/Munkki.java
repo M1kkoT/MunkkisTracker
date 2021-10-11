c290df59dab629d14cfbc641b3955f42612c5252
M1kkoT <mikko.tanhola@metropolia.fi>
2021-10-11 22:45:04 +0300

    kommentteja

--- app/src/main/java/com/example/munkkistracker/Munkki.java
+++ app/src/main/java/com/example/munkkistracker/Munkki.java
@@ -1,5 +1,18 @@
 package com.example.munkkistracker;
 
+/**
+ * Rajapinta Munkki-tyyppiselle oliolle
+ * metodit
+ * getRasva()
+ * palauttaa rasvan määräm
+ *
+ * getKcal()
+ * palauttaa kalorimäärän
+ *
+ * getSokeri()
+ * palauttaa sokerin määrän
+ */
+
 public interface Munkki {  //rajapinta munkeille
 
     public double getRasva();
