4e8d33004868f0a456b94c47626ffc31e216d6a6
sarao <saraok@metropolia.fi>
2021-10-12 12:26:57 +0300

    Javadoc

--- app/src/main/java/com/example/munkkistracker/Munkki.java
+++ app/src/main/java/com/example/munkkistracker/Munkki.java
@@ -1,5 +1,13 @@
 package com.example.munkkistracker;
 
+/**
+ * Munkki rajapinta jota käytetään luokissa
+ * Berliininmunkki
+ * Hillomunkki
+ * Munkkirinkila
+ *
+ */
+
 public interface Munkki {  //rajapinta munkeille
 
     public double getRasva();
